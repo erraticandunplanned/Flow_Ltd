2208325b6094d884045fafdc566d6fa80ab54254
David Fenneran <dfenneran@gmail.com>
2024-03-03 17:23:30 -0500

    technically works. needs cleaning

--- scenes/draw_canvas.gd
+++ scenes/draw_canvas.gd
@@ -1,19 +1,25 @@
 extends Node2D
 
-var linestart = Vector2.ZERO
-var lineend = Vector2.ZERO
-var linecolor = Color.WHITE
-var linewidth = 32.0
+var lines = []
+var circles = []
 
 func _process(delta):
 	queue_redraw()
 
+func remove_previous():
+	lines.remove_at(lines.size())
+	circles.remove_at(circles.size())
+	_draw()
+
 func call_remote_draw(s, e, c, w):
-	linestart = s
-	lineend = e
-	linecolor = c
-	linewidth = w
+	var newline = [s,e,c,w]
+	var newcircle = [s,w/2,c]
+	lines.append(newline)
+	circles.append(newcircle)
 	_draw()
 
 func _draw():
-	draw_line(linestart, lineend, linecolor, linewidth, false)
+	for i in lines:
+		draw_line(i[0], i[1], i[2], i[3], false)
+	for j in circles:
+		draw_circle(j[0], j[1], j[2])
--- scripts/test_grid.gd
+++ scripts/test_grid.gd
@@ -5,9 +5,12 @@ extends Node2D
 @onready var circle = preload("res://textures/basic_dot.png")
 @onready var gridcontainer = $grid_container
 @onready var flowcontainer = $flow_container
-@onready var cursor = $cursor
+@onready var obstaclecontainer = $obstacle_container
 @onready var canvas = $draw_canvas
+@onready var clock = $CanvasLayer/Control/TextEdit
+@onready var cursor = $cursor
 
+var time = 0
 var current_object : Node
 var instant_object : Node : 
 	set(obj) : _on_current_object_changed(obj)
@@ -21,10 +24,15 @@ func _ready():
 	for i in range(0,8):
 		for j in range(0,8):
 			var newtile = Sprite2D.new()
-			gridcontainer.add_child(newtile)
 			
-			if i > 0 and i < 7 and j == 3: newtile.texture = gridtile
-			else: newtile.texture = gridtile_black
+			
+			if i > 0 and i < 7 and j > 2 and j < 5: 
+				gridcontainer.add_child(newtile)
+				newtile.texture = gridtile
+			else: 
+				obstaclecontainer.add_child(newtile)
+				newtile.texture = gridtile_black
+			
 			newtile.name = str(i) + "," + str(j)
 			newtile.global_position = Vector2(i * 64 + 32, j * 64 + 32)
 	
@@ -35,9 +43,8 @@ func _ready():
 	#neworb.modulate.from_ok_hsl(17,73,57,255)
 	neworb.global_position = Vector2(96,224)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
-	#cursor.global_position = get_global_mouse_position()
 	
 	if Input.is_action_just_pressed("left_mouse_click"):
 		for i in flowcontainer.get_children():
@@ -50,28 +57,41 @@ func _process(delta):
 		is_dragging = false
 	
 	var mousepos = get_global_mouse_position()
+	if mousepos.x < 0 or mousepos.y < 0 or mousepos.x > 512 or mousepos.y > 512:
+		instant_object = null
+		is_dragging = false
 	
-	cursor.global_position.x = floor(mousepos.x / 64) * 64 + 32
-	cursor.global_position.y = floor(mousepos.y / 64) * 64 + 32
+	var checkpos = Vector2(floor(mousepos.x / 64) * 64 + 32, floor(mousepos.y / 64) * 64 + 32)
+	var is_valid_location = false
+	if abs(current_cursor_location.x - checkpos.x) == 64: is_valid_location = true
+	if abs(current_cursor_location.y - checkpos.y) == 64: is_valid_location = true
+	if previous_cursor_location == Vector2(0,0): is_valid_location = true
+	for i in obstaclecontainer.get_children():
+		if i.global_position == checkpos: is_valid_location = false
+	if checkpos == current_cursor_location: is_valid_location = false
+	if is_valid_location: 
+		cursor.global_position = checkpos
+		if current_cursor_location != Vector2(0,0):
+			update_clock()
 	
-	
-	if is_dragging and current_object != null:
+	if is_dragging and current_object != null and cursor.global_position != current_cursor_location:
 		current_object.global_position = cursor.global_position
-		if cursor.global_position != current_cursor_location:
-			canvas.call_remote_draw(previous_cursor_location, current_cursor_location, Color.WHITE, 32.0)
-			#draw_line(previous_cursor_location, current_cursor_location, Color(0, 1, 1, 1), 16, false)
-			print("draw?")
-	
-	if cursor.global_position != current_cursor_location: 
 		previous_cursor_location = current_cursor_location
 		current_cursor_location = cursor.global_position
-		#print("previous_cursor_location", previous_cursor_location)
-		#print("current_cursor_location", current_cursor_location)
-		#print("")
+		if previous_cursor_location != Vector2(0,0):
+			canvas.call_remote_draw(previous_cursor_location, current_cursor_location, Color.WHITE, 32.0)
+			var new_obstacle = Node2D.new()
+			obstaclecontainer.add_child(new_obstacle)
+			new_obstacle.global_position = previous_cursor_location
 
 func _on_current_object_changed(obj):
 	if obj != null:
 		current_object = obj
-		obj.modulate.a = 0.5
+		obj.modulate = Color.MEDIUM_PURPLE
 	elif current_object != null:
-		current_object.modulate.a = 1
+		current_object.modulate = Color.WHITE
+
+func update_clock():
+	time += 1
+	var text = str(time)
+	clock.text = text
