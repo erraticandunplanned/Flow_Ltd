d35c51a0dd3837a249fe125a4f06e50ae5edf63a
David Fenneran <dfenneran@gmail.com>
2024-03-04 07:14:08 -0500

    partial functionality with two nodes

--- scenes/draw_canvas.gd
+++ scenes/draw_canvas.gd
@@ -1,23 +1,37 @@
 extends Node2D
 
+@onready var grid_script = preload("res://scripts/test_grid.gd")
+
 var lines = []
 var circles = []
+var flow_nodes = []
 
 func _process(delta):
 	queue_redraw()
 
 func _draw():
 	for i in lines:
-		draw_line(i[0], i[1], i[2], i[3], false)
+		draw_line(i[0], i[1], i[2], 32, false)
 	for j in circles:
-		draw_circle(j[0], j[1], j[2])
+		draw_circle(j[0], 16, j[1])
+	for k in flow_nodes:
+		draw_circle(k[0], 28, k[1])
+
+func update_flow(d):
+	var flow_dict = d
+	flow_nodes.clear()
+	for k in flow_dict.keys():
+		var array = flow_dict.get(k)
+		var value = array.back()
+		var newflow = [value, k]
+		flow_nodes.append(newflow)
+
+func call_remote_draw(s, e, c):
+	var newline = [s,e,c]
+	var newcircle = [s,c]
+	lines.append(newline)
+	circles.append(newcircle)
 
 func remove_previous():
 	lines.remove_at(lines.size()-1)
 	circles.remove_at(circles.size()-1)
-
-func call_remote_draw(s, e, c, w):
-	var newline = [s,e,c,w]
-	var newcircle = [s,w/2,c]
-	lines.append(newline)
-	circles.append(newcircle)
--- scripts/test_grid.gd
+++ scripts/test_grid.gd
@@ -15,7 +15,13 @@ const MAPSIZE = Vector2(512,512)
 
 var time = 0
 var current_flow_node : Node
-var flow = []
+var current_flow_color : Color
+
+var flow_dict = {
+	Color.WHITE_SMOKE: [],
+	Color.LIGHT_CORAL: [],
+	#Color.MEDIUM_AQUAMARINE: []
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -34,30 +40,38 @@ func _ready():
 			newtile.name = str(i) + "," + str(j)
 			newtile.global_position = Vector2(i * 64 + 32, j * 64 + 32)
 	
-	var neworb = Sprite2D.new()
-	flowcontainer.add_child(neworb)
-	neworb.texture = circle
-	neworb.name = "start"
-	#neworb.modulate.from_ok_hsl(17,73,57,255)
-	neworb.global_position = Vector2(96,224)
-	flow.append(neworb.global_position)
-
+	var new_flow = Node2D.new()
+	flowcontainer.add_child(new_flow)
+	new_flow.name = "LIGHT_CORAL"
+	new_flow.global_position = Vector2(96,224)
+	flow_dict.get(Color.LIGHT_CORAL).append(new_flow.global_position)
+	canvas.update_flow(flow_dict)
+	#canvas.update_flow(new_flow.global_position, Color.LIGHT_CORAL)
+	
+	var new_flow_2 = Node2D.new()
+	flowcontainer.add_child(new_flow_2)
+	new_flow_2.name = "WHITE_SMOKE"
+	new_flow_2.global_position = Vector2(96,288)
+	flow_dict.get(Color.WHITE_SMOKE).append(new_flow_2.global_position)
+	canvas.update_flow(flow_dict)
+	
 
 func _process(delta):
-	## REVERSE OF THE FLOW ARRAY. USED LATER ON
-	
 	
 	## ON MOUSE CLICK, SET NEAREST FLOW NODE TO CURRENT
 	if Input.is_action_just_pressed("left_mouse_click"):
 		for i in flowcontainer.get_children():
 			if get_global_mouse_position().distance_to(i.global_position) < 32:
 				current_flow_node = i
-				current_flow_node.modulate = Color.MEDIUM_PURPLE
+				for j in flow_dict.keys():
+					var key = flow_dict[j]
+					if key.back() == i.global_position:
+						current_flow_color = j
 	
 	## ON MOUSE RELEASE, SET CURRENT FLOW NODE TO NULL
 	if Input.is_action_just_released("left_mouse_click"):
-		current_flow_node.modulate = Color.WHITE
 		current_flow_node = null
+		current_flow_color = Color.WHITE
 	
 	## SET "CURSOR_SNAP" TO THE NEAREST GRID LOCATION OF MOUSE
 	var mousepos = get_global_mouse_position()
@@ -67,13 +81,13 @@ func _process(delta):
 		
 		## SET "CURSOR_CLOCK" TO ONLY VALID TILES
 		var is_valid = true
-		if flow.has(cursor_snap.global_position): 
+		if flow_dict.get(current_flow_color).has(cursor_snap.global_position): 
 			var reverse_flow : Array
-			reverse_flow.append_array(flow)
+			reverse_flow.append_array(flow_dict.get(current_flow_color))
 			reverse_flow.reverse()
 			if reverse_flow.size() > 1:
 				if reverse_flow[1] == cursor_snap.global_position:
-					flow.remove_at(flow.size()-1)
+					flow_dict.get(current_flow_color).remove_at(flow_dict.get(current_flow_color).size()-1)
 					canvas.remove_previous()
 					is_valid = true
 				else: is_valid = false
@@ -89,11 +103,13 @@ func _process(delta):
 			
 			## UPDATE FLOW POSITION AND DRAW LINES
 			current_flow_node.global_position = cursor_clock.global_position
-			flow.append(current_flow_node.global_position)
+			flow_dict.get(current_flow_color).append(current_flow_node.global_position)
 			var reverse_flow : Array
-			reverse_flow.append_array(flow)
+			reverse_flow.append_array(flow_dict.get(current_flow_color))
 			reverse_flow.reverse()
-			canvas.call_remote_draw(reverse_flow[1], reverse_flow[0], Color.WHITE, 32.0)
+			canvas.call_remote_draw(reverse_flow[1], reverse_flow[0], current_flow_color)
+			canvas.update_flow(flow_dict)
+			#canvas.update_flow(reverse_flow[0], current_flow_color)
 			
 			## ADVANCE CLOCK
 			time += 1
